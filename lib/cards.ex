6abd8382a042793f91ffd59b7fd50c26fe6e246d
Leo Vargas <leocarvalho.vargas@gmail.com>
2020-12-25 20:17:42 -0300

    Tune Cards.create_deck method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,36 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    nested_cards =
+      for suit <- suits, value <- values do
+        "#{value} #{suit}"
+      end
+
+    List.flatten(nested_cards)
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
